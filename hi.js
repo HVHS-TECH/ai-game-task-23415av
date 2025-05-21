12ca409287b4e9bd7c5802730cf06735c30c4447
23415av <23415av@hvhs.school.nz>
2025-05-21 00:02:15 +0000

    fix the start and pause and die

---
+++ hi.js
@@ -0,0 +1,11 @@
+/*
+please make a code for a snake game. 
+The points to collect are mouse emojis. 
+It needs to be able to go for until you die. 
+You need to die if you hit yourself. You need to die if you hit the wall. 
+You need to have a point counter. 
+You need to make it so that the snake gets slightly bigger with every point it gains. 
+It can only gain a point if the head goes over the space. 
+It needs to be placed on a pixel grid.
+*/
+
